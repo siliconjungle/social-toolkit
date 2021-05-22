e2294789e38b81a7d735f95d81b6935f6a0f9486
siliconjungle <siliconjungle.io@gmail.com>
2021-05-22 18:57:17 +1000

    feat(theme): add defaults

---
+++ config/theme.js
@@ -0,0 +1,57 @@
+const theme = {
+	fontSize: {
+		f1: '6rem',
+		f2: '5rem',
+		f3: '3rem',
+		f4: '2.25rem',
+		f5: '1.5rem',
+		f6: '1.25rem',
+		f7: '1rem',
+		f8: '.875rem',
+		f9: '.75rem',
+	},
+	lineHeight: {
+		solid: 1,
+		title: 1.25,
+		copy: 1.5,
+	},
+	tracking: {
+		tracked: '.1em',
+		trackedTight: '-.05em',
+		trackedMega: '.25em',
+	},
+	spacing: {
+		none: '0',
+		xs: '.25rem',
+		s: '.5rem',
+		m: '1rem',
+		l: '2rem',
+		xl: '4rem',
+		xxl: '8rem',
+		xxxl: '16rem',
+	},
+	borderRadius: {
+		br1: '0',
+		br2: '.125rem',
+		br3: '.25rem',
+		br4: '.5rem',
+		br5: '1rem',
+		br100: '100%',
+		brPill: '9999px',
+	},
+	shadows: {
+		shadow1: 'box-shadow: 0px 0px 4px 2px rgba( 0, 0, 0, 0.2 );',
+		shadow2: 'box-shadow: 0px 0px 8px 2px rgba( 0, 0, 0, 0.2 );',
+		shadow3: 'box-shadow: 2px 2px 4px 2px rgba( 0, 0, 0, 0.2 );',
+		shadow4: 'box-shadow: 2px 2px 8px 0px rgba( 0, 0, 0, 0.2 );',
+		shadow5: 'box-shadow: 4px 4px 8px 0px rgba( 0, 0, 0, 0.2 );',
+	},
+	colors: {
+		primary: '#4267B2',
+		grey: '#898F9C',
+		white: '#ffffff',
+		black: '#000000',
+	},
+}
+
+export default theme
