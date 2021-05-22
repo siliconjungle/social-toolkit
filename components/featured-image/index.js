d8786b224642c66d876e825b49674be03758ec17
siliconjungle <71357672+siliconjungle@users.noreply.github.com>
2021-05-22 23:11:14 +1000

    ref(featured-image): add comment for image size

--- components/featured-image/index.js
+++ components/featured-image/index.js
@@ -4,7 +4,9 @@ const myLoader = ({ src, width, quality }) => {
   return `${src}?w=${width}&q=${quality || 75}`
 }
 
-
+// Currently this is defaulting to the image size that Facebook uses for featured links.
+// For a comprehensive list of image sizes for different social media sites
+// Check here: https://sproutsocial.com/insights/social-media-image-sizes-guide/
 const FeaturedImage = ({ src, alt }) => {
   return (
     <Image
