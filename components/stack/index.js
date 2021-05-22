35eb2a8ef8d4b5d6e986d40cc0825835ceec6ebd
siliconjungle <siliconjungle.io@gmail.com>
2021-05-22 19:55:03 +1000

    feat(stack): add component

---
+++ components/stack/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import theme from '../../config/theme'
+
+const Container = styled.div`
+  width: 100%;
+  ${({ spacing }) => {
+    return (
+      spacing && `
+        & > * {
+          margin-bottom: ${spacing};
+          &:last-child {
+            margin-bottom: ${theme.spacing.none};
+          }
+        }
+      `
+    )
+  }}
+`
+
+const Stack = ({ children, spacing }) => {
+  return (
+    <Container spacing={spacing}>
+      {children}
+    </Container>
+  )
+}
+
+export default Stack
